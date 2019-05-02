ed894341573a3f58bc4be2adc95c3f7d73403dab
Shabir Ahmad <shabir@jejunu.ac.kr>
2019-05-02 14:46:24 +0900

    Initial UI updates to Dashboard

---
+++ markersdata.js
@@ -0,0 +1,79 @@
+marker_callback([{
+	"type": "Watson IoT",
+	 "color_code":"dd4b39",
+	"devices": [{
+		"id": "1",
+		"properties": {
+			"title": 5.4,
+			"link": "",
+			"url": "http://watson-iot/dev1",
+			"status": "",
+			"date":"11-7-2019"
+		},
+		"geometry": {
+			"type": "marker",
+			"coordinates": [33.4890, 126.4983]
+		}
+	},
+    {
+		"id": "2",
+		"properties": {
+			"title": 5.4,
+			"link": "",
+			"url": "http://watson-iot/dev2",
+			"status": "pending",
+			 "date":"11-7-2019"
+		},
+		"geometry": {
+			"type": "marker",
+			"coordinates": [34.8377129, 123.8582433]
+		}
+	},
+    {
+		"id": "3",
+		"properties": {
+			"title": 5.4,
+			"link": "",
+			"url": "http://watson-iot/dev3",
+			"status": "",
+			"date":"11-7-2019"
+		},
+		"geometry": {
+			"type": "marker",
+			"coordinates": [34.8377129, 124.8582433]
+		}
+	},
+    {
+		"id": "4",
+		"properties": {
+			"title": "Watson IoT Device 3",
+			"link": "",
+			"url": "http://watson-iot/dev4",
+			"status": "",
+			"date":"11-7-2019"
+		},
+		"geometry": {
+			"type": "marker",
+			"coordinates": [34.4890, 121.4983]
+		}
+	}],
+	"id": "ibmwattson1"
+}, {
+	"type": "OCF Device",
+	 "color_code":"00c0ef",
+	"devices": [{
+		"id": "1",
+		"properties": {
+			"title": "OCF Device 2",
+			"link": "",
+			"url": "http://ocf-iot/dev1",
+			"status": "",
+			"date":"11-7-2019"
+		},
+		"geometry": {
+			"type": "marker",
+			"coordinates": [35.252115, 128.378728]
+		}
+	}],
+	"id": "ocf-dev"
+}]);
